b17fb2d7d5f5ccd7bba23e8a091d42b7ed6d7e6f
Ubuntu <ubuntu@ip-172-31-6-29.ap-northeast-2.compute.internal>
2017-04-05 01:26:54 +0000

    part 6 pdf 추가

--- router/route.js
+++ router/route.js
@@ -1,10 +1,30 @@
+var mysql = require('mysql');
+var db_config = require('../db');
 
-module.exports = function(app, fs, passport, upload)
+module.exports = function(app)
 {
      app.get('/',function(req,res){
         res.render('index',{
             title: "insta_project"
         });
+     });
+
+     app.get('/test',function(req,res){
+       var db = mysql.createConnection(db_config);
+       var sql = 'SELECT * FROM `post` WHERE 1';
+        db.query(sql, function(err,rows){
+          if(err){
+            console.error('mysql connection error');
+            console.error(err);
+            throw err;
+          }
+          var posts = rows;
+          var isWrite = 0;
+          res.render('test',{
+            posts:posts
+          });
+          db.end();
+       });
      });
 
 };
