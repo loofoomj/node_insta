bfdaf95f0ed19308d5bb3e494dcba588739ec359
Ubuntu <ubuntu@ip-172-31-6-29.ap-northeast-2.compute.internal>
2017-04-13 12:58:25 +0000

    part 7 추가, 회원가입 추가

--- router/route.js
+++ router/route.js
@@ -27,4 +27,43 @@ module.exports = function(app)
        });
      });
 
+
+     app.get('/register',function(req,res){
+        res.render('register',{
+
+        });
+     });
+
+     app.post('/register_check',function(req,res){
+       var db = mysql.createConnection(db_config);
+       var id = req.body.id;
+       var pw = req.body.pw;
+       var repw = req.body.repw;
+       var sql = 'SELECT count(*) AS idCount FROM `user` WHERE `user_id`= ?';
+        db.query(sql, [id], function(err,rows){
+          if(err){
+            console.error('mysql connection error');
+            console.error(err);
+            throw err;
+          }
+          var count = rows[0].idCount;
+          if(count!==0){
+            db.end();
+            res.send('<script type="text/javascript">alert("아이디가 존재합니다.");location.href="/register"</script>');
+          } else {
+            var reg_date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+            var sql_2 = 'INSERT INTO `user`(`user_id`, `user_pw`, `reg_date`) VALUES (?, ?, ?)';
+            db.query(sql_2, [id, pw, reg_date], function(err){
+              if(err){
+                console.error('mysql connection error');
+                console.error(err);
+                throw err;
+              }
+            });
+            db.end();
+            res.send('<script type="text/javascript">alert("회원가입 완료!");location.href="/"</script>');
+          }
+       });
+
+     });
 };
